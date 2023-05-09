97ebbdb26d0c4f6bf20eb279f306473ab6cc9049
Katey Basye <kbasye+github@gmail.com>
2023-05-08 22:52:10 -0700

    only updating highlighted since last update

--- quotion/app.js
+++ quotion/app.js
@@ -1,14 +1,16 @@
+// Import necessary libraries
 require('dotenv').config();
-
 const axios = require('axios');
 const db = require('./database');
+const { insertUpdate } = require('./database');
 
+// Set API keys and endpoints
 const apiKey = process.env.RWAPI_KEY;
 const apiUrl = 'https://readwise.io/api/v2/export/'; 
-
 const dallEapiKEY = process.env.dallEapiKEY;
 const dallEUrl = 'https://api.openai.com/v1/images/generations';
 
+// Initialize Notion client
 const { Client } = require("@notionhq/client");
 const { OpenAIApi } = require('openai');
 
@@ -16,19 +18,20 @@ const { OpenAIApi } = require('openai');
 const notion = new Client({
   auth: process.env.NOTION_API_KEY,
 });
+
+// Define functions
 async function getLastUpdate() {
     return new Promise((resolve, reject) => {
       db.get(`SELECT * FROM Updates ORDER BY date DESC LIMIT 1`, (err, row) => {
         if (err) {
           reject(err);
         } else {
-            const timestamp = new Date(row.date).getTime();
+            const timestamp = new Date(row.date).toISOString();
             resolve(timestamp);
         }
       });
     });
   }
-  
 async function createInput(document, coverImageUrl) {
     try {
       const newInput = {
@@ -44,6 +47,9 @@ async function createInput(document, coverImageUrl) {
           },
           URL: {
             url: document.source_url,
+          },
+          "Cover URL": {
+            url: coverImageUrl,
           },
           Type: {
             select: {
@@ -70,7 +76,6 @@ async function createInput(document, coverImageUrl) {
       console.error("Error creating input page:", error.message);
     }
   }
-
 async function createQuote(highlight, inputID, coverImageUrl) {
   try {
     const newQuote = {
@@ -83,6 +88,14 @@ async function createQuote(highlight, inputID, coverImageUrl) {
                 },
               },
             ],
+          },
+          "Cover URL": {
+            url: coverImageUrl,
+          },
+          "Created Readwise": {
+            date: {
+              start: highlight.highlighted_at,
+            }
           },
           Source: {
             relation: [
@@ -112,7 +125,6 @@ async function createQuote(highlight, inputID, coverImageUrl) {
     console.error("Error creating quote page:", error.message);
   }
 }
-
 async function processTags(highlightTags, quoteID) {
     try {
       const databaseId = process.env.TOPICS_DATABASE_ID;
@@ -174,7 +186,6 @@ async function processTags(highlightTags, quoteID) {
       console.error('Error processing tags:', error.message);
     }
   }
-  
 async function addConnection(inputID, author) {
 try {
     const databaseId = process.env.CONNECTIONS_DATABASE_ID;
@@ -261,36 +272,49 @@ async function generateImage(prompt, size = "1024x1024", n = 1, responseFormat =
       console.error("Error generating image:", error.message);
     }
   }  
-  
-  
+
+
+// Fetch data from Readwise API
 axios
   .get(apiUrl, {
     headers: {
       Authorization: `Token ${apiKey}`
     },
     params: {
-        pageCursor,
+      // updatedAfter: '2023-05-02T16:41:53.186Z'
     },
   })
   .then(async (response) => {
     const lastUpdate = await getLastUpdate();
-    console.log(response)
-    console.log("••••••••••••••••••••••••••BIG DIVIDER••••••••••••••••••••••••••")
-    console.log(response.data.results[0])
+
+    const start = new Date("5/8/2023, 10:04 PM")
+    console.log("Start: " + start);
+
     const results = response.data.results;
     for (const result of results) { 
-        if (new Date(result.highlighted_at).getTime() > lastUpdate) {
-            const url = await generateImage("An impressionist painting of " + result.title);
-            const inputID = await createInput(result, url);
-            await processTags(result.book_tags, inputID);
-            await addConnection(inputID, result.author);
-            const highlights = result.highlights;
-            for (const highlight of highlights) {
+        let shouldCreateInput = false;
+        for (const highlight of result.highlights) {
+          // console.log(new Date(highlight.created_at) > start)
+          if (new Date(highlight.created_at) > start) {
+            shouldCreateInput = true;
+            break;
+          }
+        }
+        if (shouldCreateInput) {
+          const url = await generateImage("An impressionist painting of " + result.title);
+          const inputID = await createInput(result, url);
+          await processTags(result.book_tags, inputID);
+          await addConnection(inputID, result.author);
+          const highlights = result.highlights;
+          for (const highlight of highlights) {
+            const highlightedAt = new Date(highlight.created_at);
+            if (highlightedAt > start) {
               const url = await generateImage("An impressionist painting of " + result.title);
               const quote = await createQuote(highlight, inputID, url);
               const highlightTags = highlight.tags;
-              await processTags(highlightTags, quote.id);        
+              await processTags(highlightTags, quote.id);
             }
+          }
         }
     }
   })
